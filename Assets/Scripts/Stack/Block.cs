c92b52f6fce5068ac20b3390d4ba3299e39e18dd
Gori <wnsrjf0922@gmail.com>
2025-02-20 12:44:42 +0900

    [ FEAT ] 스택 게임 종료 조건 추가

--- Assets/Scripts/Stack/Block.cs
+++ Assets/Scripts/Stack/Block.cs
@@ -27,6 +27,11 @@ public class Block : MonoBehaviour
     
     private void OnTriggerStay2D(Collider2D collision)
     {
+        if (collision.name == "FailCollider")
+        {
+            StackGameManager.Instance.GameOver();
+        }
+        
         if (isLanded && !isFalling)
         {
             StackGameManager.Instance.GameOver();
